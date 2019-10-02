9831ff3adb5121010c4e4c0e30b0d31d7409b52e
snylonue <snylonue@gmail.com>
2019-10-02 13:24:47 +0800

    small change

--- mpv-bilbili/bilibili2mpv.py
+++ mpv-bilbili/bilibili2mpv.py
@@ -7,12 +7,12 @@ import argparse
 def main(url,hwdec):
 	rescmd=subprocess.run(f'you-get {url} -u',shell=True,stdout=subprocess.PIPE)
 	getout=rescmd.stdout.decode().strip().split('\r\n')
-	for x,v in enumerate(getout):
-		if v=='Real URLs:':
-			urls=getout[x+1:]
-			break
-	else:
+	try:
+		sp=getout.index('Real URLs:')
+	except ValueError:
 		raise OSError(f'can not get real url of {url} ')
+	else:
+		urls=getout[sp+1:]
 	assert urls
 	if len(urls)==2:
 		cmd=f"""mpv "{urls[0]}" --audio-file="{urls[1]}" --referrer="https://www.bilibili.com" --no-ytdl \
@@ -26,7 +26,7 @@ def main(url,hwdec):
 if __name__ == '__main__':
 	parser=argparse.ArgumentParser(description='play bilibili vedio with mpv')
 	parser.add_argument('url',type=str,help='vedio url')
-	parser.add_argument('-d','--hwdec',nargs=1,type=str,help='hardware decode opitions,use "mpv --hwdec=help" \
-to get more information',default=['no'])
+	parser.add_argument('-d','--hwdec',nargs=1,type=str,default=['no'],help='hardware decode opitions,use "mpv --hwdec=help" \
+to get more information')
 	a=parser.parse_args()
 	main(url=a.url,hwdec=a.hwdec[0])
