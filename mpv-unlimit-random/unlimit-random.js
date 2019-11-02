e3a024f388e4eae0baa9f4759439784e2f5bf6f5
snylonue <snylonue@gmail.com>
2019-11-02 09:55:08 +0800

    fix playlist will get longer while playing

--- mpv-unlimit-random/unlimit-random.js
+++ mpv-unlimit-random/unlimit-random.js
@@ -2,20 +2,24 @@
 
 var random=false;
 
+function zrandom(nmax) {
+	return Math.round(Math.random()*nmax);
+}
 function random_play() {
+	var pl,pos,setpos;
 	if (random) {
-		var plct=mp.get_property('playlist-count');
-		var pos;
+		pl=mp.get_property_number('playlist-count');
+		pos=mp.get_property_number('playlist-pos');
 		do {
-			mp.command('playlist-shuffle');
-			pos=mp.get_property('playlist-pos-1');
-		} while(plct==pos);
+			setpos=zrandom(pl);
+		} while(setpos===pos);
+		mp.set_property('playlist-pos',setpos.toString());
 	}
 }
 function random_play_control() {
 	random=!random;
-	mp.osd_message('Random: '+(random&&'yes'||'no'));
+	mp.osd_message('Random: '+(random?'yes':'no'));
 }
 
-mp.register_event('start-file',random_play)
+mp.register_event('end-file',random_play)
 mp.add_key_binding('y','random_control',random_play_control)
