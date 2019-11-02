c811b10e75be820b7a0015fc94d78ebaf189568d
snylonue <snylonue@gmail.com>
2019-11-02 09:58:10 +0800

    small improvement

--- mpv-unlimit-random/unlimit-random.js
+++ mpv-unlimit-random/unlimit-random.js
@@ -3,18 +3,18 @@
 var random=false;
 
 function random_play() {
+	var pl,pos;
 	if (random) {
-		var plct=mp.get_property('playlist-count');
-		var pos;
+		pl=mp.get_property_number('playlist-count');
 		do {
 			mp.command('playlist-shuffle');
-			pos=mp.get_property('playlist-pos-1');
-		} while(plct==pos);
+			pos=mp.get_property_number('playlist-pos-1');
+		} while(pl===pos);
 	}
 }
 function random_play_control() {
 	random=!random;
-	mp.osd_message('Random: '+(random&&'yes'||'no'));
+	mp.osd_message('Random: '+(random?'yes':'no'));
 }
 
 mp.register_event('start-file',random_play)
