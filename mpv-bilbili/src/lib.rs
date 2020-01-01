ad5dff4df86e349ed60639a68c4a77da6dc83526
snylonue <snylonue@gmail.com>
2020-01-01 18:42:29 +0800

    get real urls from you-get with json Parsing will be "easier" and offer more information though the code is far more than before. By the way, you-get will output json data with log, and it may cause some issues and make parsing harder.

--- mpv-bilbili/src/lib.rs
+++ mpv-bilbili/src/lib.rs
@@ -1,28 +1,55 @@
+use serde_json;
+use serde_json::Value;
 use std::process;
 use std::process::Stdio;
 
 type Res<T> = Result<T, String>;
 
 pub fn get_url(orig_url: &String) -> Res<Vec<String>> {
-    let cmd = match process::Command::new("you-get").arg(orig_url).arg("-u").output() {
+    let (you_get_stdout, you_get_stderr) = match process::Command::new("you-get").arg(orig_url).arg("--json").output() {
         Ok(r) => {
-            r
+            let stdout = match String::from_utf8(r.stdout) {
+                Ok(r) => r,
+                Err(e) => return Err(format!("{:?}", e)),
+            };
+            let stderr = match String::from_utf8(r.stderr) {
+                Ok(r) => r,
+                Err(e) => String::from(format!("Failed to read stderr: {:?}", e)),
+            };
+            (stdout, stderr)
         },
         Err(e) => return Err(format!("{:?}", e)),
     };
-    let sto = match String::from_utf8(cmd.stdout) {
-        Ok(r) => {
-            r
-        },
-        Err(e) => return Err(format!("{:?}", e)),
+    //process you-get output as json only
+    let stos = you_get_stdout.split("{")
+        .map(|x| { x.trim().to_string() + "{" })
+        .fold(String::new(), |mul, x| { mul + &x });
+    let stos = stos.trim_end_matches('{');
+    let json_stdout = serde_json::from_str(stos);
+    //res = json_stdout['streams']['flv']['src']
+    let res = match json_stdout {
+        Ok(j) => match j {
+            Value::Object(o) => match o["streams"].clone() {
+                Value::Object(o) => match o["flv"].clone() {
+                    Value::Object(o) => match o["src"].clone() {
+                        Value::String(s) => vec![s],
+                        Value::Array(a) => a.iter().map(|v| {
+                            match v {
+                                Value::String(s) => s.clone(),
+                                _ => String::new(),
+                            }
+                        }).collect(),
+                        _ => return Err(format!("Failed to parse stdout as url in src\nstdout: {}\nstderr: {}", stos, you_get_stderr)),
+                    }
+                    _ => return Err(format!("Failed to parse stdout as url in flv\nstdout: {}\nstderr: {}", stos, you_get_stderr)),
+                }
+                _ => return Err(format!("Failed to parse stdout as url in streams\nstdout: {}\nstderr: {}", stos, you_get_stderr)),
+            }
+            _ => return Err(format!("Failed to parse stdout as url at top\nstdout: {}\nstderr: {}", stos, you_get_stderr)),
+        }
+        Err(e) => return Err(format!("Failed to deserialize stdout: {:?}", e)),
     };
-    let stos = sto.clone();
-    let mut stos = stos.split("\n").map(|x| { x.trim().to_string() });
-    if let None = stos.position(|x| { x.trim() == "Real URLs:" }) {
-        return Err(format!("{}", "failed to parse stdout as url"));
-    }
-    let res: Vec<_> = stos.filter(|x| { x.trim() != "" }).map(|x| { x.trim().to_string() }).collect();
-    Ok(res)
+    Ok(res.clone())
 }
 pub fn play_with_mpv(orig_url: &String, sto: Stdio) -> Res<()> {
     let url = get_url(orig_url)?;
