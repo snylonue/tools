dac5e0b9587f58a313640f0a1f1f92392a56781b
snylonue <snylonue@gmail.com>
2020-01-04 13:37:18 +0800

    fix new animes can not be played new animes' urls only have dash mode, while old animes' urls don't have dash mode ...

--- mpv-bilbili/src/lib.rs
+++ mpv-bilbili/src/lib.rs
@@ -6,14 +6,19 @@ use std::panic;
 
 type Res<T> = Result<T, String>;
 
+pub enum Website {
+    Bilibili(bool),
+}
+
 pub struct MediaInfo {
     pub urls: Vec<String>,
     pub title: String,
     pub referrer: String,
+    pub website: Website,
 }
 
 pub fn get_url(orig_url: &String) -> Res<(MediaInfo)> {
-    let (you_get_stdout, you_get_stderr) = match process::Command::new("you-get").arg(orig_url).arg("--json").output() {
+    let (stdout, stderr) = match process::Command::new("you-get").arg(orig_url).arg("--json").output() {
         Ok(r) => {
             let stdout = match String::from_utf8(r.stdout) {
                 Ok(r) => r,
@@ -27,36 +32,36 @@ pub fn get_url(orig_url: &String) -> Res<(MediaInfo)> {
         },
         Err(e) => return Err(format!("{:?}", e)),
     };
-    //process you-get output as json only
-    let stos = you_get_stdout.split("{")
-        .map(|x| { x.trim().to_string() + "{" })
-        .fold(String::new(), |mul, x| { mul + &x });
-    let stos = stos.trim_end_matches('{');
-    let json_stdout = match serde_json::from_str(stos) {
+    let json_stdout = match serde_json::from_str(&*stdout) {
         Ok(j) => match j {
             Value::Object(o) => o,
-            _ => return Err(format!("Failed to parse stdout as url at top\nstdout: {}\nstderr: {}", stos, you_get_stderr)),
+            _ => return Err(format!("Failed to parse stdout as url at top\nstdout: {}\nstderr: {}", stdout, stderr)),
         },
         Err(e) => return Err(format!("Failed to deserialize stdout: {:?}", e)),
     };
+    let website = Website::Bilibili(true);
     let urls =  panic::catch_unwind(|| {
-        Ok(json_stdout["streams"]["flv"].clone())
+        Ok(json_stdout["streams"]["dash-flv"].clone())
     }).unwrap_or_else(|e| {
-        return Err(format!("Failed to parse stdout as url\nerror: {:?}\nstdout: {}\nstderr: {}", e, stos, you_get_stderr));
+        return Err(format!("Failed to parse stdout as url\nerror: {:?}\nstdout: {}\nstderr: {}", e, stdout, stderr));
     })?;
     let urls = panic::catch_unwind(|| {
         match urls["src"].clone() {
             Value::String(s) => Ok(vec![s]),
             Value::Array(a) => Ok(a.iter().map(|v| {
-                match v {
+                match v {   
                     Value::String(s) => s.clone(),
+                    Value::Array(a) => match a[0].clone() {
+                        Value::String(s) => s,
+                        _ => String::new(),
+                    },
                     _ => String::new(),
                 }
             }).collect()),
-            _ => Err("No url is found".to_string())
+            _ => Err(format!("No url is found\nstdout: {}\nstderr: {}", stdout, stderr))
         }
     }).unwrap_or_else(|e| {
-        return Err(format!("Failed to parse stdout as url\nerror: {:?}\nstdout: {}\nstderr: {}", e, stos, you_get_stderr));
+        return Err(format!("Failed to parse stdout as url\nerror: {:?}\nstdout: {}\nstderr: {}", e, stdout, stderr));
     })?;
     // referrer = json_output['extra']['referer']
     let referrer = match json_stdout["extra"].clone() {
@@ -71,20 +76,29 @@ pub fn get_url(orig_url: &String) -> Res<(MediaInfo)> {
         Value::String(s) => s,
         _ => String::new(),
     };
-    Ok(MediaInfo { urls, referrer, title })
+    Ok(MediaInfo { urls, referrer, title, website })
 }
 pub fn play_with_mpv(media_info: MediaInfo, sto: Stdio) -> Res<()> {
-    let MediaInfo { urls, title, referrer } = media_info;
+    let MediaInfo { urls, title, referrer, website } = media_info;
     let mut cmd = process::Command::new("mpv");
-    for i in urls.iter() {
-        cmd.arg(i);
-    }
+    match website {
+        Website::Bilibili(b) => {
+            if b {
+                cmd.arg(urls[0].clone())
+                    .arg(format!("--audio-file={}", urls[1]));
+            } else {
+                for i in urls.iter() {
+                    cmd.arg(i);
+                }
+            }
+        },
+    };
     cmd.arg(format!("--referrer={}", referrer))
         .arg(format!("--title={}", title))
         .arg("--merge-files")
         .arg("--no-ytdl")
         .stdout(sto)
         .spawn().expect("Failed to spawn child process")
-        .wait_with_output().expect("Failed to run command");
+        .wait().expect("Failed to run command");
     Ok(())
 }
