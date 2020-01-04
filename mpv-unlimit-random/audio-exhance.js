3149d96c36d0487df5d6c0cb8422792f6db94c91
snylonue <snylonue@gmail.com>
2020-01-04 14:48:29 +0800

    add audio-exhance.js

---
+++ mpv-unlimit-random/audio-exhance.js
@@ -0,0 +1,41 @@
+'use_strict';
+
+var audio_exhance = {
+	random : false,
+	loop : false,
+	pasue : false,
+};
+
+function pl_ending() {
+	return mp.get_property_number('playlist-pos-1') === mp.get_property_number('playlist-count');
+}
+function random_play() {
+	var pl,pos;
+	if (audio_exhance.random) {
+		do {
+			mp.command('playlist-shuffle');
+		} while(pl_ending());
+	}
+}
+function pasue_on_finish() {
+	if (audio_exhance.pasue) {
+		mp.command('cycle pause');
+	}
+}
+function random_play_control() {
+	audio_exhance.random = !audio_exhance.random;
+	mp.osd_message('Random: ' + (audio_exhance.random ? 'yes' : 'no'));
+}
+function loop_list_control() {
+	audio_exhance.loop = !audio_exhance.loop;
+	mp.osd_message('Loop list: ' + (audio_exhance.loop ? 'yes' + (audio_exhance.random ? '(but will not work with random)' : '') : 'no'));
+}
+function pause_on_finish_control() {
+	audio_exhance.pasue = !audio_exhance.pasue;
+	mp.osd_message('Pause on finish: ' + (audio_exhance.pasue ? 'yes' : 'no'));
+}
+
+mp.register_event('start-file', random_play);
+mp.register_event('end-file', pasue_on_finish);
+mp.add_key_binding('y', 'random_control', random_play_control);
+mp.add_key_binding('p', 'pause_on_finish_control', pause_on_finish_control);
